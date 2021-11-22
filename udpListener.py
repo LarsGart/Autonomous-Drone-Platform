bcbd4e40c865bb29cb6e78f4cd643c46c2c25142
Jerin Abraham <abrahamjerin98@gmail.com>
2021-11-21 20:17:14 -0500

    Update udpListener.py

--- udpListener.py
+++ udpListener.py
@@ -1,6 +1,6 @@
 import socket
 
-localIP     = "localhost"
+localIP     = "0.0.0.0"
 localPort   = 44444
 
 sock = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
@@ -8,4 +8,5 @@ sock.bind((localIP, localPort))
 
 print("Listening for UDP packets...")
 while(True):
-    print(sock.recvfrom(1024)[0].decode())
+    data = sock.recvfrom(1024)
+    print(data[0].decode())
