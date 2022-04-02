b425fd9ed5177a3ecbba80011e7493222925381b
Jerin Abraham <abrahamjerin98@gmail.com>
2022-04-02 17:09:57 -0500

    Added startup script for receiver reading
    Imported ibus library and wrote receiver read script. Still need to add code that stores rx out data in memory

---
+++ startup_scripts/readRX.py
@@ -0,0 +1,22 @@
+from asyncio.windows_events import NULL
+import time
+import serial
+import ibus
+
+# Define the UART
+uart = serial.Serial(
+    port = "/dev/ttyTHS1",
+    baudrate = 115200,
+    bytesize = serial.EIGHTBITS,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE
+)
+# Wait a second to let the port initialize
+time.sleep(1)
+
+def storeRxData(rxDataIn):
+    # TODO: add stuff to write rx data to memory
+    pass
+
+ib = ibus.IBUS(uart, sensor_types=NULL, servo_cb=storeRxData, do_log=False)
+ib.start_loop()
