faafae85748ecf6c3c7d9578e9eb8408b7fa2cf3
Jerin Abraham <abrahamjerin98@gmail.com>
2022-04-03 02:21:41 -0500

    Update readRX.py
    added comment and removed whitespace for assignments

--- startup_scripts/readRX.py
+++ startup_scripts/readRX.py
@@ -5,11 +5,11 @@ import ibus
 
 # Define the UART
 uart = serial.Serial(
-    port = "/dev/ttyTHS1",
-    baudrate = 115200,
-    bytesize = serial.EIGHTBITS,
-    parity = serial.PARITY_NONE,
-    stopbits = serial.STOPBITS_ONE
+    port="/dev/ttyTHS1",
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE
 )
 # Wait a second to let the port initialize
 time.sleep(1)
@@ -18,5 +18,6 @@ def storeRxData(rxDataIn):
     # TODO: add stuff to write rx data to memory
     pass
 
+# Start IBUS reading loop
 ib = ibus.IBUS(uart, sensor_types=None, servo_cb=storeRxData, do_log=False)
 ib.start_loop()
