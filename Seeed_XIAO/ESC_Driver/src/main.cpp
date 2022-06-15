f65db9b0445d4a79dd23ed95bd18e38e3fda8f7d
Jerin Abraham <abrahamjerin98@gmail.com>
2022-06-15 00:48:25 -0400

    Updated speed controller
    Made the speed controller read data faster

--- Seeed_XIAO/ESC_Driver/src/main.cpp
+++ Seeed_XIAO/ESC_Driver/src/main.cpp
@@ -7,6 +7,33 @@ Servo m[4];
 // Define speeds
 int speed[4] = {1000, 1000, 1000, 1000};
 
+// Define speed byte array
+char speedBytes[8];
+int bytesRead = 0;
+bool readingData = false;
+bool speedUpdated = false;
+
+void readSerialData() {
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == '<') {
+      readingData = true;
+      bytesRead = 0;
+      memset(speedBytes, 0, 8);
+    }
+    else if (c == '>' && readingData) {
+      readingData = false;
+      speedUpdated = true;
+    }
+    else {
+      if (readingData && bytesRead < 8) {
+        speedBytes[bytesRead] = c;
+        bytesRead++;
+      }
+    }
+  }
+}
+
 void setup() {
   // Attach ESCs to the pins
   m[0].attach(4);
@@ -20,21 +47,17 @@ void setup() {
 
 void loop() {
   // Read speed input if available
-  if (Serial.available() > 0) {
-    char buffer[16];
-    size_t bytesRead = Serial.readBytesUntil('\n', buffer, 16);
-    if (bytesRead >= 8) {
-      for (int i = 0; i < 4; ++i) {
-        // Convert byte stream to motor speeds
-        speed[i] = (buffer[2 * i] << 8 | buffer[2 * i + 1]);
+  readSerialData();
 
-        // Constrain speed values to 1000-2000 range
-        if (speed[i] < 1000) {
-          speed[i] = 1000;
-        }
-        else if (speed[i] > 2000) {
-          speed[i] = 2000;
-        }
+  // Convert byte array to motor speeds
+  if (speedUpdated) {
+    speedUpdated = false;
+    for (int i = 0; i < 4; ++i) {
+      int mSpeed = (speedBytes[2 * i] << 8 | speedBytes[2 * i + 1]);
+
+      // Only update speed if it's in the 1000-2000 range
+      if (mSpeed >= 1000 && mSpeed <= 2000) {
+        speed[i] = mSpeed;
       }
     }
   }
