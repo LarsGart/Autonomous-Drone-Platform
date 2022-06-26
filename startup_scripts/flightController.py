b1914518b6319cc650ea1f49b7b08ef81147dd12
Jerin Abraham <abrahamjerin98@gmail.com>
2022-06-26 00:07:26 -0400

    Designed basic flight controller

--- startup_scripts/flightController.py
+++ startup_scripts/flightController.py
@@ -1,7 +1,33 @@
+'''
+Author: Jerin Abraham
+
+This is the main flight controller script
+
+CHANNEL MAPPINGS
+
+    CH1 (ROLL): LEFT = 1000, RIGHT = 2000
+    CH2 (PITCH): DOWN = 1000, UP = 2000
+    CH3 (THROTTLE): DOWN = 1000, UP = 2000
+    CH4 (YAW): LEFT = 1000, RIGHT = 2000
+
+MOTOR MAPPINGS
+ 
+    3  ^  0
+     \_|_/
+     |   |
+     |___|
+     /   \
+    2     1
+
+'''
+
+# Python modules
 from time import sleep
+from numpy import clip
 import serial
 
-from ibus_rewrite import IBUS
+# Custom modules
+from ibus import IBUS
 
 # Define the UARTs
 uart1 = serial.Serial(
@@ -25,12 +51,12 @@ ib = IBUS(uart=uart1)
 
 def outputSpeeds(speeds):
     uart2.write(bytearray(
-        60,
+        60, # Sends a '<'
         speeds[0] >> 8, speeds[0] & 255,
         speeds[1] >> 8, speeds[1] & 255,
         speeds[2] >> 8, speeds[2] & 255,
         speeds[3] >> 8, speeds[3] & 255,
-        62
+        62 # Sends a '>'
     ))
 
 def main():
@@ -38,9 +64,23 @@ def main():
         # Read RX values
         rxData = ib.readIBUS()
 
+        rxDataConv = [
+            (rxData[0] - 1500) / 500,
+            (rxData[1] - 1500) / 500,
+            (rxData[2] - 1000) / 1000,
+            (rxData[3] - 1500) / 500 
+        ]
+
         if (rxData is not None):
             # Generate motor speeds
-            print(rxData)
+            rawSpeeds = [
+                0.5 * rxDataConv[2] + 0.2 * rxDataConv[0] - 0.2 * rxDataConv[1] + 0.2 * rxDataConv[3],
+                0.5 * rxDataConv[2] + 0.2 * rxDataConv[0] + 0.2 * rxDataConv[1] - 0.2 * rxDataConv[3],
+                0.5 * rxDataConv[2] - 0.2 * rxDataConv[0] + 0.2 * rxDataConv[1] + 0.2 * rxDataConv[3],
+                0.5 * rxDataConv[2] - 0.2 * rxDataConv[0] - 0.2 * rxDataConv[1] - 0.2 * rxDataConv[3]
+            ]
+            outSpeeds = clip([1000 * i + 1000 for i in rawSpeeds], 1000, 2000)
+            outputSpeeds(outSpeeds)
 
 if __name__ == '__main__':
     sleep(1)
