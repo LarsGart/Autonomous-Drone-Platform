f7b93c5c320692a2733ecdb1149c8d1569af1a8c
Chronoshifting <54747779+LarsGart@users.noreply.github.com>
2022-06-27 20:10:15 -0400

    [ADD] Bézier curve computation & viewer
    [ADD] Bézier curve computation & viewer

---
+++ Aruco/bezierComputation.py
@@ -0,0 +1,115 @@
+import numpy as np
+from imutils.video import VideoStream
+import argparse
+import imutils
+import time
+import cv2
+import sys
+import matplotlib.pyplot as plt
+from scipy.special import comb
+
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+arucoTag = 'DICT_ARUCO_ORIGINAL'
+
+# verify that the supplied ArUCo tag exists and is supported by OpenCV
+if ARUCO_DICT.get(arucoTag, None) is None:
+    print("[INFO] ArUCo tag of '{}' is not supported".format(arucoTag))
+    sys.exit(0)
+
+# load the ArUCo dictionary and grab the ArUCo parameters
+print("[INFO] detecting '{}' tags...".format(arucoTag))
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[arucoTag])
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+
+def get_bezier_parameters(X, Y, degree=6):
+
+    """
+    Least square quadratic bezier fit using penrose pseudoinverse.
+    Parameters:
+    X: array of x data.
+    Y: array of y data. Y[0] is the y point for X[0].
+    degree: degree of the Bézier curve e.g. 2 for quadratic, 3 for cubic.
+    """
+
+    if degree < 1:
+        raise ValueError('degree must be 1 or greater.')
+
+    if len(X) != len(Y):
+        raise ValueError('X and Y must be of the same length.')
+
+    if len(X) < degree + 1:
+        raise ValueError(f'There must be at least {degree + 1} points to '
+                         f'determine the parameters of a degree {degree} curve. '
+                         f'Got only {len(X)} points.')
+
+    def bpoly(n, t, k):
+        # Bernstein polynomial when a = 0 and b = 1
+        return t ** k * (1 - t) ** (n - k) * comb(n, k)
+
+    def bmatrix(T):
+        # Bernstein matrix for Bézier curves
+        return np.matrix([[bpoly(degree, t, k) for k in range(degree + 1)] for t in T])
+
+    def least_square_fit(points, M):
+        M_ = np.linalg.pinv(M)
+        return M_ * points
+
+    T = np.linspace(0, 1, len(X))
+    M = bmatrix(T)
+    points = np.array(list(zip(X, Y)))
+    final = least_square_fit(points, M).tolist()
+    final[0] = [X[0], Y[0]]
+    final[len(final)-1] = [X[len(X)-1], Y[len(Y)-1]]
+    return final
+
+def bernstein_poly(i, n, t):
+    return comb(n, i) * ( t**(n-i) ) * (1 - t)**i
+
+def bezier_curve(points, nTimes=50):
+    """
+    Given a set of control points, return the
+    Bezier curve defined by the control points.
+    nTimes is the number of time steps, defaults to 50
+    """
+
+    nPoints = len(points)
+    xPoints = np.array([p[0] for p in points])
+    yPoints = np.array([p[1] for p in points])
+
+    t = np.linspace(0.0, 1.0, nTimes)
+
+    polynomial_array = np.array([bernstein_poly(i, nPoints-1, t) for i in range(0, nPoints)])
+
+    xvals = np.dot(xPoints, polynomial_array)
+    yvals = np.dot(yPoints, polynomial_array)
+
+    return xvals, yvals
+    
