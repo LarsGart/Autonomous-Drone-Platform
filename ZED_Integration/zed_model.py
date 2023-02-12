2ee4605b62d53c7b48b604e46a55e9d03fe05b83
larsgart <larsgart@buffalo.edu>
2023-02-11 19:30:26 -0500

    [ADD] get_euler()

--- ZED_Integration/zed_model.py
+++ ZED_Integration/zed_model.py
@@ -21,6 +21,7 @@ import numpy as np
 import math
 import serial
 import logging
+import math
 
 
 '''
@@ -142,6 +143,7 @@ class ZedModel():
 
         return data
 
+
     '''
     Retrieves the orientation data from the IMU sensor using the Zed Camera API and returns the filtered orientation quaternion.
     The orientation data is represented as a 4-element quaternion in the form [Ox, Oy, Oz, Ow].
@@ -163,6 +165,23 @@ class ZedModel():
         else:
             self.logger.warning("IMU data has not been updated")
             return None
-
+        
+    
     def get_euler(self):
-        pass #TODO: make this https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        q = self.get_quaternion()
+        x, y, z, w = q[0], q[1], q[2], q[3]
+
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return [roll_x, pitch_y, yaw_z] # in radians
