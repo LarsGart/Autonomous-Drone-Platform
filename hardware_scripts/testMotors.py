1c96170fd02d6f8af0b6befe8acfcedf9f4ae53e
laga-odoo <larsgart@buffalo.edu>
2023-03-06 00:05:24 -0500

    [REFAC] testMotors

--- hardware_scripts/testMotors.py
+++ hardware_scripts/testMotors.py
@@ -1,34 +1,28 @@
 import serial
 
-uart2 = serial.Serial(
-    port="/dev/ttyS0",
-    baudrate=115200
-)
+# Define UART
+uart2 = serial.Serial(port="/dev/ttyS0", baudrate=115200)
 
-# Split speeds into MSB and LSB for each speed and send byte stream to speed controller via UART2
+# Function to output speeds
 def outputSpeeds(speeds):
-    uart2.write(bytearray([
-        60, # Sends a '<'
-        (speeds[0] >> 8) & 255, speeds[0] & 255,
-        (speeds[1] >> 8) & 255, speeds[1] & 255,
-        (speeds[2] >> 8) & 255, speeds[2] & 255,
-        (speeds[3] >> 8) & 255, speeds[3] & 255,
-        62 # Sends a '>'
-    ]))
+    # Create byte array to send to speed controller via UART
+    byte_arr = bytearray([60] + [(speeds[i] >> 8) & 255 for i in range(4)] + [speeds[i] & 255 for i in range(4)] + [62])
+    uart2.write(byte_arr)
 
+# Main function to receive motor speeds and output them
 def main():
-	while 1:
-		spd = input("Enter motor speed: ")
-		try:
-			speedList = [int(x) for x in spd.split(",")]
-		except ValueError:
-			print("Invalid Speeds!")
-			speedList = [1000] * 4
-
-		outputSpeeds(speedList)
+    while True:
+        spd = input("Enter motor speed: ")
+        try:
+            speedList = [int(x) for x in spd.split(",")]
+        except ValueError:
+            print("Invalid Speeds!")
+            speedList = [1000] * 4
+        outputSpeeds(speedList)
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         outputSpeeds([1000] * 4)
+        
