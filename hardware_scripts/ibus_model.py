63e601799e876fec9fffa56b20ad03543e215ca7
laga-odoo <larsgart@buffalo.edu>
2023-03-05 23:25:09 -0500

    [REFAC] IBus

--- hardware_scripts/ibus_model.py
+++ hardware_scripts/ibus_model.py
@@ -7,47 +7,42 @@ Code taken from https://github.com/house4hack/circuitpython-ibus
 I removed the parts that weren't needed for this project and did some refactoring
 '''
 
-# Protocol constants
-PROTOCOL_SERVO = 0x40
-PROTOCOL_CHANNELS = 14
-PROTOCOL_OVERHEAD = 3
-PROTOCOL_LENGTH = 0x20
+class IBus:
+    PROTOCOL_SERVO = 0x40
+    PROTOCOL_CHANNELS = 14
+    PROTOCOL_OVERHEAD = 3
+    PROTOCOL_LENGTH = 0x20
 
-class IBus():
     def __init__(self, uart):
         self.uart = uart
 
     def readUART(self):
-        data = self.uart.read(1)
+        data = None
         while data is None:
             data = self.uart.read(1)
         return data
 
-    def checksum(self, arr, initial):
-        sum = initial
-        for val in arr:
-            sum += val
+    @staticmethod
+    def checksum(arr, initial):
+        sum = initial + sum(arr)
         checksum = 0xFFFF - sum
-        chA = checksum >> 8
-        chB = checksum & 0xFF
-        return chA, chB
-    
+        return checksum >> 8, checksum & 0xFF
+
     def readIBUS(self):
         data = self.readUART()
 
         expectedLen = data[0] - 1
-        if (expectedLen < PROTOCOL_LENGTH and expectedLen >= PROTOCOL_OVERHEAD):
+        if PROTOCOL_OVERHEAD <= expectedLen < PROTOCOL_LENGTH:
             dataArr = bytearray(expectedLen)
             totalRead = self.uart.readinto(dataArr)
-            if (totalRead == expectedLen):
+            if totalRead == expectedLen:
                 cmd = dataArr[0] & 0xF0
 
-                chA1 = dataArr[-1]
-                chB1 = dataArr[-2]
-
+                chA1, chB1 = dataArr[-1], dataArr[-2]
                 chA2, chB2 = self.checksum(dataArr[:-2], expectedLen + 1)
-                if (chA1 == chA2 and chB1 == chB2):
-                    if (cmd == PROTOCOL_SERVO):
-                        return [(dataArr[2 * i + 2] << 8) | dataArr[2 * i + 1] for i in range(PROTOCOL_CHANNELS)]
-        
+                if chA1 == chA2 and chB1 == chB2:
+                    if cmd == self.PROTOCOL_SERVO:
+                        return [(dataArr[2 * i + 2] << 8) | dataArr[2 * i + 1] for i in range(self.PROTOCOL_CHANNELS)]
+
         return None
+    
