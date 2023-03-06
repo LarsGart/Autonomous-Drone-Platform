10bc1318c88e25f47c2c2526882d89142c698d27
laga-odoo <larsgart@buffalo.edu>
2023-03-05 21:51:58 -0500

    [REFAC] PID

--- hardware_scripts/pid_model.py
+++ hardware_scripts/pid_model.py
@@ -2,17 +2,13 @@
 Author: Jerin Abraham
 
 This class acts as a generic PID loop
-
 It can be used for any PID purpose
 '''
 import numpy as np
 
-'''
-Class to instantiate a PID object
-'''
-class PID():
+
+class PID:
     '''
-    Constructor for PID class
     Initializes the pid coefficients and errors
 
     PARAMETERS:
@@ -21,54 +17,22 @@ class PID():
         kD (float): Derivative coefficient of the PID loop
         limit (float): Absolute value of the maximum output from the PID loop
     '''
-    def __init__(self, kP = 0, kI = 0, kD = 0, limit = 100):
-        self.ctrlInput = 0
-        self.worldInput = 0
-        self.output = 0
-
-        self.err = 0
-        self.deltaErr = 0
-        self.errSum = 0
-        self.prevErr = 0
-
-        self.kP = kP
-        self.kD = kI
-        self.kI = kD
-
-        self.limit = limit
+    def __init__(self, kP=0, kI=0, kD=0, limit=100):
+        self.ctrlInput, self.worldInput, self.output = 0, 0, 0
+        self.err, self.deltaErr, self.errSum, self.prevErr = 0, 0, 0, 0
+        self.kP, self.kD, self.kI, self.limit = kP, kI, kD, limit
 
     def __calcErr(self):
-        # Calculate P error
         self.err = self.worldInput - self.ctrlInput
-
-        # Calculate I error if kI is greater than 0
-        if (self.kI > 0):
-            errSum = np.clip(
-                self.errSum + self.err,
-                -self.limit / self.kI,
-                self.limit / self.kI
-            )
-
-        # Calculate D error and update previous error
-        self.deltaErr = self.err - self.prevErr
-        self.prevErr = self.err
+        if self.kI > 0:
+            self.errSum = np.clip(self.errSum + self.err, -self.limit/self.kI, self.limit/self.kI)
+        self.deltaErr, self.prevErr = self.err - self.prevErr, self.err
         
     def __calcPID(self):
-        # 
-        self.output = np.clip(
-            self.kP * self.err + self.kI * self.errSum + self.kD * self.deltaErr,
-            -self.limit,
-            self.limit
-        )
+        self.output = np.clip(self.kP*self.err + self.kI*self.errSum + self.kD*self.deltaErr, -self.limit, self.limit)
     
-    '''
-    Reset PID errors
-    '''
     def resetError(self):
-        self.err = 0
-        self.deltaErr = 0
-        self.errSum = 0
-        self.prevErr = 0
+        self.err, self.deltaErr, self.errSum, self.prevErr = 0, 0, 0, 0
 
     '''
     Calculate PID output from desired value and measured world value
@@ -81,10 +45,8 @@ class PID():
         (float): pid output
     '''
     def calc(self, ctrlInput, worldInput):
-        self.ctrlInput = ctrlInput
-        self.worldInput = worldInput
-
+        self.ctrlInput, self.worldInput = ctrlInput, worldInput
         self.__calcErr()
         self.__calcPID()
-
         return self.output
+    
