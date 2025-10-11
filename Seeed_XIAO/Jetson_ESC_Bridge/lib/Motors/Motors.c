89f08b0c5ddb36e7ce4dfe1acc5da3c94bc482e7
Jerin Abraham <abrahamjerin98@gmail.com>
2025-10-11 21:28:34 +0900

    Update Motors library to implement DShot - Switched from using TCC0 with 4 compare channels to TC4 with 1 compare   channel - Added a function to send a generic command value to a motor with DShot

--- Seeed_XIAO/Jetson_ESC_Bridge/lib/Motors/Motors.c
+++ Seeed_XIAO/Jetson_ESC_Bridge/lib/Motors/Motors.c
@@ -1,8 +1,40 @@
 #include "Motors.h"
 
-#define PWM_FREQUENCY 50 // 50Hz for standard ESCs
-#define CLK_PRESCALER 16 // Clock division factor for GCLK_TCC0
-#define PWM_PERIOD (F_CPU / CLK_PRESCALER / PWM_FREQUENCY) // Timer period for 50Hz with prescaler 16
+#define TC_CYCLES_PER_BIT 3     // Number of timer cycles per DShot bit
+#define DSHOT_BAUD        300E3 // Dshot baudrate in bits/s
+#define NUM_DSHOT_BITS    16    // Number of bits in a DShot packet
+#define SPEED_CMD_BASE    48    // Command value for a motor speed of 0
+
+// Pin number of each motor
+#define MOTOR_1_PIN 8
+#define MOTOR_2_PIN 11
+#define MOTOR_3_PIN 10
+#define MOTOR_4_PIN 2
+
+// Constants
+static const uint8_t MOTOR_BIT_POSITIONS[4] = { // Bit positions to be used for writing the OUTTGL register
+  MOTOR_1_PIN,
+  MOTOR_2_PIN,
+  MOTOR_3_PIN,
+  MOTOR_4_PIN
+};
+static const uint8_t TOGGLE_SEQUENCES[2][TC_CYCLES_PER_BIT] = { // Pin toggle sequence for each DShot bit
+  {1, 1, 0},
+  {1, 0, 1}
+};
+static const uint32_t ALL_MOTOR_BITS =  (1 << MOTOR_1_PIN) |
+                                        (1 << MOTOR_2_PIN) |
+                                        (1 << MOTOR_3_PIN) | 
+                                        (1 << MOTOR_4_PIN);
+static const uint8_t NUM_TOGGLE_WORDS = NUM_DSHOT_BITS * TC_CYCLES_PER_BIT;
+static const uint16_t TC4_CC_VAL = (uint16_t) (F_CPU / (TC_CYCLES_PER_BIT * DSHOT_BAUD));
+
+// Variables
+static uint8_t dshot_data_bits[4][NUM_DSHOT_BITS];
+static bool dshot_data_bits_valid[4];
+static uint32_t toggle_data[NUM_TOGGLE_WORDS];
+static uint8_t toggle_data_i = 0;
+static bool toggle_data_valid = false;
 
 // Initialize the Motors instance
 Motors_t motors = {
@@ -10,24 +42,59 @@ Motors_t motors = {
   .debug_mode = false
 };
 
-// Constants
-static const int CLKS_PER_MICROSECOND = (F_CPU / CLK_PRESCALER / 1000000); // Clock cycles per microsecond
-static const int CC_1000US = (F_CPU / CLK_PRESCALER / 1000); // TCC CC value for a 1000 us pulse width
-
 // ----------------------------------------------------------------------
 // PRIVATE FUNCTIONS
 // ----------------------------------------------------------------------
 /*!
-  \brief Convert a speed value to timer compare/capture value
-  \param speed Speed value (0-1999)
-  \return Corresponding timer compare/capture value
+  \brief Convert data into an array of DShot bits and update the dshot_data_bits array
+  \param data Command being sent to a motor (0-2047)
+  \param motor Index for the target motor (0-3)
 */
-static uint32_t convert_speed_to_cc(int speed) {
-  return CLKS_PER_MICROSECOND * (int) (0.5 * speed + 1000); // Convert speed (0-1999) to timer ticks
+static void generate_dshot_bitstream(uint16_t data, uint8_t motor) {
+  /*
+    The data needs to be left-shifted by 1 first to account for the telemetry bit before
+    the CRC calculation can be performed
+  */
+  uint16_t shifted_data = data << 1;
+  uint16_t crc = (shifted_data ^ (shifted_data >> 4) ^ (shifted_data >> 8)) & 0x0F;
+  uint16_t dshot_data = (shifted_data << 4) | crc;
+  for (int i = 0; i < NUM_DSHOT_BITS; i++) {
+    dshot_data_bits[motor][i] = (dshot_data >> i) & 0x01;
+  }
+  dshot_data_bits_valid[motor] = true;
 }
 
 /*!
-  \brief Configure pins to be used for PWM output
+  \brief Update the toggle register data array based on the DShot data for each valid motor
+*/
+static void update_toggle_data_array(void) {
+  // Clear the toggle data array
+  for (int i = 0; i < NUM_TOGGLE_WORDS; i++) {
+    toggle_data[i] = 0;
+  };
+
+  // Update the toggle data array based on the valid DShot data for each motor
+  for (int m = 0; m < 4; m++) {
+    if (!dshot_data_bits_valid[m])
+      continue;
+    dshot_data_bits_valid[m] = false;
+
+    uint8_t motor_bit_pos = MOTOR_BIT_POSITIONS[m];
+    for (int i = 0; i < NUM_DSHOT_BITS; i++) {
+      uint8_t toggle_data_base_i = TC_CYCLES_PER_BIT * i;
+      uint8_t dshot_data_bit = dshot_data_bits[m][i];
+      uint8_t *bit_toggle_sequence = TOGGLE_SEQUENCES[dshot_data_bit];
+      for (int t = 0; t < TC_CYCLES_PER_BIT; t++) {
+        toggle_data[toggle_data_base_i + t] |= (bit_toggle_sequence[t] << motor_bit_pos);
+      }
+    }
+  }
+  toggle_data_i = 0;
+  toggle_data_valid = true;
+}
+
+/*!
+  \brief Configure pins to be used for DShot output
 
   Motor 1: PA8
   Motor 2: PA11
@@ -36,123 +103,104 @@ static uint32_t convert_speed_to_cc(int speed) {
 */
 static void configure_pins(void) {
   // Disable pin mux
-  PORT->Group[PORTA].PINCFG[2].bit.PMUXEN = 0;
-  PORT->Group[PORTA].PINCFG[8].bit.PMUXEN = 0;
-  PORT->Group[PORTA].PINCFG[10].bit.PMUXEN = 0;
-  PORT->Group[PORTA].PINCFG[11].bit.PMUXEN = 0;
+  PORT->Group[PORTA].PINCFG[MOTOR_1_PIN].bit.PMUXEN = 0;
+  PORT->Group[PORTA].PINCFG[MOTOR_2_PIN].bit.PMUXEN = 0;
+  PORT->Group[PORTA].PINCFG[MOTOR_3_PIN].bit.PMUXEN = 0;
+  PORT->Group[PORTA].PINCFG[MOTOR_4_PIN].bit.PMUXEN = 0;
 
-  // Set pin directions to output
-  PORT->Group[PORTA].DIRSET.reg = PORT_PA02 |
-                                  PORT_PA08 |
-                                  PORT_PA10 |
-                                  PORT_PA11;
+  // Set pin directions to output and set them low
+  PORT->Group[PORTA].DIRSET.reg = ALL_MOTOR_BITS;
+  PORT->Group[PORTA].OUTCLR.reg = ALL_MOTOR_BITS;
 }
 
 /*!
-  \brief Configure TCC0 for four unique 50Hz PWM outputs
+  \brief Configure TC4 to overflow at 3x the DShot baudrate
 */
-static void configure_timers(void) {
-  // Enable TCC0 clocks
-  PM->APBCMASK.reg |= PM_APBCMASK_TCC0;                 // Enable APBC clock for TCC0
-  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(GCM_TCC0_TCC1) |  // Select the TCC0 and TCC1 clock
-                      GCLK_CLKCTRL_GEN_GCLK0 |          // Use generic clock generator 0
-                      GCLK_CLKCTRL_CLKEN;               // Enable the clock
+static void configure_timer(void) {
+  // Enable TC4 clocks
+  PM->APBCMASK.reg |= PM_APBCMASK_TC4;                // Enable APBC clock for TC4
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(GCM_TC4_TC5) |  // Select the TC4 and TC5 clock
+                      GCLK_CLKCTRL_GEN_GCLK0 |        // Use generic clock generator 0
+                      GCLK_CLKCTRL_CLKEN;             // Enable the clock
   while (GCLK->STATUS.bit.SYNCBUSY);
 
-  // Reset TCC0 and wait for synchronization
-  TCC0->CTRLA.bit.ENABLE = 0;
-  while (TCC0->SYNCBUSY.bit.ENABLE);
-  TCC0->CTRLA.bit.SWRST = 1;
-  while (TCC0->CTRLA.bit.SWRST || TCC0->SYNCBUSY.bit.SWRST);
+  // Reset TC4 and wait for synchronization
+  TC4->COUNT16.CTRLA.bit.ENABLE = 0;
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);
+  TC4->COUNT16.CTRLA.bit.SWRST = 1;
+  while (TC4->COUNT16.CTRLA.bit.SWRST || TC4->COUNT16.STATUS.bit.SYNCBUSY);
 
-  // Configure TCC0 for PWM generation
-  TCC0->CTRLA.reg = TCC_CTRLA_PRESCALER_DIV16;  // Set prescaler to 16
-  TCC0->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;       // Normal PWM mode
-  TCC0->PER.reg = PWM_PERIOD;                   // Set period for desired frequency
-  while (TCC0->SYNCBUSY.bit.PER || TCC0->SYNCBUSY.bit.WAVE);
+  // Configure TC4 counting frequency
+  TC4->COUNT16.CTRLA.reg =  TC_CTRLA_WAVEGEN_MFRQ | // Match Frequency Generation to control period with CC
+                            TC_CTRLA_MODE_COUNT16;  // Use 16-bit counter mode
+  TC4->COUNT16.CC->reg = TC4_CC_VAL;                // Set CC to give 3x baudrate frequency
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);
 
-  // Set initial duty cycles to 1000 microseconds (motors off)
-  TCC0->CC[0].reg = CC_1000US; // Motor 1
-  TCC0->CC[1].reg = CC_1000US; // Motor 2
-  TCC0->CC[2].reg = CC_1000US; // Motor 3
-  TCC0->CC[3].reg = CC_1000US; // Motor 4
-  while (TCC0->SYNCBUSY.bit.CC0 || TCC0->SYNCBUSY.bit.CC1 || TCC0->SYNCBUSY.bit.CC2 || TCC0->SYNCBUSY.bit.CC3);
+  // Enable TC4 overflow interrupt
+  TC4->COUNT16.INTENSET.bit.OVF = 1;
 
-  // Enable TCC0 interrupts for overflow and match/capture channels
-  TCC0->INTENSET.reg =  TCC_INTENSET_MC0 |  // Compare/Capture Channel 0
-                        TCC_INTENSET_MC1 |  // Compare/Capture Channel 1
-                        TCC_INTENSET_MC2 |  // Compare/Capture Channel 2
-                        TCC_INTENSET_MC3 |  // Compare/Capture Channel 3
-                        TCC_INTENSET_OVF;   // Overflow
-
-  // Enable TCC0
-  TCC0->CTRLA.bit.ENABLE = 1;
-  while (TCC0->SYNCBUSY.bit.ENABLE);
+  // Enable TC4
+  TC4->COUNT16.CTRLA.bit.ENABLE = 1;
+  while (TC4->COUNT16.STATUS.bit.SYNCBUSY);
 }
 
 // ----------------------------------------------------------------------
 // PUBLIC FUNCTIONS
 // ----------------------------------------------------------------------
 /*!
-  \brief Initialize motor control by configuring pins and timers
+  \brief Initialize motor control by configuring pins and timer
 */
 void motors_init(void) {
-  NVIC_DisableIRQ(TCC0_IRQn);
-  NVIC_ClearPendingIRQ(TCC0_IRQn);
-  NVIC_SetPriority(TCC0_IRQn, 0);
+  NVIC_DisableIRQ(TC4_IRQn);
+  NVIC_ClearPendingIRQ(TC4_IRQn);
+  NVIC_SetPriority(TC4_IRQn, 0);
 
   configure_pins();
-  configure_timers();
+  configure_timer();
 
-  NVIC_EnableIRQ(TCC0_IRQn);
+  NVIC_EnableIRQ(TC4_IRQn);
+}
+
+/*!
+  \brief Send a DShot command to a motor
+  \param cmd Command to be sent (0-2047)
+  \param motor Index of the target motor (0-3)
+*/
+void send_motor_cmd(const uint16_t cmd, const uint8_t motor) {
+  // Don't update the DShot bitstream if we're currently sending DShot data
+  if (toggle_data_valid)
+    return;
+  toggle_data_valid = true;
+  generate_dshot_bitstream(cmd, motor);
+  update_toggle_data_array();
 }
 
 /*!
   \brief Update the pulse widths for all motors
   \param speeds Array of 4 speed values (0-1999) for motors
 */
-void set_motor_speed(const uint16_t* speeds) {
-  TCC0->CCB[0].reg = convert_speed_to_cc(speeds[0]);
-  TCC0->CCB[1].reg = convert_speed_to_cc(speeds[1]);
-  TCC0->CCB[2].reg = convert_speed_to_cc(speeds[2]);
-  TCC0->CCB[3].reg = convert_speed_to_cc(speeds[3]);
+void send_motor_speeds(const uint16_t* speeds) {
+  // Don't update the DShot bitstream if we're currently sending DShot data
+  if (toggle_data_valid)
+    return;
+  toggle_data_valid = true;
+  for (int m = 0; m < 4; m++) {
+    generate_dshot_bitstream(speeds[m] + SPEED_CMD_BASE, m);
+  }
+  update_toggle_data_array();
 }
 // ----------------------------------------------------------------------
 // INTERRUPT HANDLER
 // ----------------------------------------------------------------------
-void TCC0_Handler(void) {
-  // Set motor 1 pin low (PA08) on Compare/Capture Channel 0 interrupt
-  if (TCC0->INTFLAG.bit.MC0) {
-    TCC0->INTFLAG.reg |= TCC_INTFLAG_MC0;
-    PORT->Group[PORTA].OUTCLR.reg = PORT_PA08;
-  }
-
-  // Set motor 2 pin low (PA11) on Compare/Capture Channel 1 interrupt
-  if (TCC0->INTFLAG.bit.MC1) {
-    TCC0->INTFLAG.reg |= TCC_INTFLAG_MC1;
-    PORT->Group[PORTA].OUTCLR.reg = PORT_PA11;
-  }
-
-  // Set motor 3 pin low (PA10) on Compare/Capture Channel 2 interrupt
-  if (TCC0->INTFLAG.bit.MC2) {
-    TCC0->INTFLAG.reg |= TCC_INTFLAG_MC2;
-    PORT->Group[PORTA].OUTCLR.reg = PORT_PA10;
-  }
-
-  // Set motor 4 pin low (PA02) on Compare/Capture Channel 3 interrupt
-  if (TCC0->INTFLAG.bit.MC3) {
-    TCC0->INTFLAG.reg |= TCC_INTFLAG_MC3;
-    PORT->Group[PORTA].OUTCLR.reg = PORT_PA02;
-  }
-
-  // Set all motor pins high at the start of the next PWM cycle if armed and not in debug mode
-  if (TCC0->INTFLAG.bit.OVF) {
-    TCC0->INTFLAG.reg |= TCC_INTFLAG_OVF;
-    if (motors.armed && !motors.debug_mode) {
-      PORT->Group[PORTA].OUTSET.reg = PORT_PA02 |
-                                      PORT_PA08 |
-                                      PORT_PA10 |
-                                      PORT_PA11; 
+void TC4_Handler(void) {
+  // Write the output toggle register on Overflow interrupt
+  if (TC4->COUNT16.INTFLAG.bit.OVF) {
+    TC4->COUNT16.INTFLAG.reg = TC_INTFLAG_OVF;
+    if (toggle_data_valid) {
+      PORT->Group[PORTA].OUTTGL.reg = toggle_data[toggle_data_i++];
+      if (toggle_data_i == NUM_TOGGLE_WORDS) {
+        toggle_data_valid = false;
+      }
     }
   }
 }
--- Seeed_XIAO/Jetson_ESC_Bridge/lib/Motors/Motors.h
+++ Seeed_XIAO/Jetson_ESC_Bridge/lib/Motors/Motors.h
@@ -1,11 +1,35 @@
 /*!
   \file Motors.h
-  \brief 4-in-1 ESC interface
+  \brief 4-in-1 ESC interface using DShot
 
-  This library uses TCC0 to create four 50Hz PWM outputs to control the
-  four motors for the drone
+  The library uses TC4 for timing the Dshot bits and the timer period is set so that it runs
+  at 3x the DShot bitrate (e.g. 900kHz for DShot300)
 
-  \todo Update this library to use/support DSHOT
+  This is done because the DShot protocol uses pulse length to determine a 1 or 0, with a 0
+  being roughly 1/3 of the duty cycle and a 1 being roughly 2/3 of the duty cycle
+
+  The timer overflows 3x per bit and the state of the motor pins is modified on each overflow
+  to send a bit
+
+  An example diagram of sending a 0 and then a 1 is illustrated below:
+
+                           0            1
+                           |            |
+                      /----------\/-----------\
+                      _____       _________
+          Output: ____|   |_______|       |____
+                  -----------------------------
+                  |   |   |   |   |   |   |   |
+  Overflow count: 0   1   2   3   4   5   6   7
+
+  More information about the DShot protocol can be found here:
+  https://brushlesswhoop.com/dshot-and-bidirectional-dshot/
+
+  To minimize register writes on each timer overflow, the OUTTGL register is the only one
+  written instead of the OUTSET and OUTCLR registers
+
+  This library sends a DShot command/speed to the ESC only when the corresponding function
+  is called
 */
 #ifndef MOTORS_H
 #define MOTORS_H
@@ -26,7 +50,8 @@ extern Motors_t motors;
 
 // Functions
 void motors_init(void);
-void set_motor_speed(const uint16_t* speeds);
+void send_motor_cmd(const uint16_t cmd, const uint8_t motor);
+void send_motor_speeds(const uint16_t* speeds);
 
 #ifdef __cplusplus
 }
